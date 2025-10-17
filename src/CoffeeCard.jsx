ea20c34660d89a5f5088e3c0f8ed3ef5cb3e2dcb
Sebastian Giedziun <seba.nstory@gmail.com>
2025-10-17 14:33:05 +0200

    add conditional rendering to CoffeeCard

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -1,12 +1,24 @@
 function CoffeeCard({ name, type, description, isNew }) {
-
+function isCoffeeNew() {
+    // if (isNew == true) {
+    //     return <p>Ta kawa jest nowa w ofercie</p>
+    // } else {
+    //     return null
+    // }
+    return isNew === true ? <p>Ta kawa jest nowa w ofercie</p> : null
+}
     return (
         <div className="coffe-card">
             <h2>{name}</h2>
             <p>{type}</p>
             <p>{description}</p>
+            {
+                // isCoffeeNew()
+                isNew === true ? <p>Ta kawa jest nowa w ofercie</p> : null
+            }
         </div>
     )
+
 }
 
 export default CoffeeCard
